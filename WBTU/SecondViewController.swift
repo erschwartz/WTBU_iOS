d54d20abd3abe25a81761ea180f2b17bdd7ca48e
bakpakin <calsrose@gmail.com>
2015-10-06 20:05:17 -0400

    Initial Commit

---
+++ WBTU/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WBTU
+//
+//  Created by Calvin Rose on 10/6/15.
+//  Copyright © 2015 Calvin Rose. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
