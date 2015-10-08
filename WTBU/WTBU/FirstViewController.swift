a10de9dd46973af7972b24e67927dd7b8d65976f
Erika Schwartz <erikaschwartz@comcast.net>
2015-10-08 12:50:54 -0400

    User profile in progress

---
+++ WTBU/WTBU/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WTBU
+//
+//  Created by Admin on 10/6/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
